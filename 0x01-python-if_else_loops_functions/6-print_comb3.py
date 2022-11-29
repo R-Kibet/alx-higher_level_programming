76e904086acdd6c1e2432364fc83e3d8b3bc8dcd
root <richkibz@gmail.com>
2022-11-29 16:53:02 +0300

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-for i in range (0, 90):
-	if i % 10 > i / 10:
-		if i == 89:
-			print(f"{i}")
-		else:
-			print(f"{i:02d}", end = ', ')
+for i in range(0, 90):
+    if i % 10 > i / 10:
+        if i == 89:
+            print("{}".format(i))
+        else:
+            print("{:02d}".format(i), end=', ')

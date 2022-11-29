6d56b7b01a83354e570a182aadae3f2baf61988a
root <richkibz@gmail.com>
2022-11-29 17:15:38 +0300

    ASCII in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 for i in reversed(range(97, 123)):
-	print(f"{chr(i if (i % 2 == 0) else (i - 32))}", end = '')
+    print("{}".format(chr(i if (i % 2 == 0) else (i - 32))), end='')

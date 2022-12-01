b89a9a9b8b7ccf327405bb610a5781b673421006
root <richkibz@gmail.com>
2022-12-01 11:04:03 +0300

    print number and list of arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} arguments:".formar(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for a in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, a))
+
+            i += 1

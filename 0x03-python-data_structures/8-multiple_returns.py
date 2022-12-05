0c00d091d5da90530dd86bec3258f0151a3d4cd5
root <richkibz@gmail.com>
2022-12-05 18:42:51 +0300

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,6 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first = ""
     if sentence == "":
-        first = "None"
-    for i in range(0, 1):
-        first = first + sentence[i]
-    return (length, first)
+        return (0, None)
+    return (length, sentence[0])

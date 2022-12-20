612193a0d302acea2dee6a02b726fbbcef2cc35e
root <richkibz@gmail.com>
2022-12-21 01:30:52 +0300

    Bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" a  Python class MagicClass that does exactly
-the same as the following Python bytecode:"""
+"""Python class MagicClass that does exactly
+the same as the following Python bytecode"""
 import math
 
 
 class MagicClass:
-    """ constructor"""
+    """ class constructor"""
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    """ area"""
+    """finding the  area"""
     def area(self):
         return self.__radius ** 2 * math.pi
 
-    """circumference"""
+    """calculation of the circumference"""
     def circumference(self):
         return 2 * math.pi * self.

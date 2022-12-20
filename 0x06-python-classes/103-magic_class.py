1c01f3b8747694d50c5b01fa446cf63719c2833c
root <richkibz@gmail.com>
2022-12-21 01:17:40 +0300

    Bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+""" a  Python class MagicClass that does exactly
+the same as the following Python bytecode:"""
+
 
 import math
 
 
 class MagicClass:
-
+    """ constructor"""
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:

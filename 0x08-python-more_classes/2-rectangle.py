ea26a7e6851c599f689f77a9964a977d629d3685
root <richkibz@gmail.com>
2023-01-03 21:40:05 +0300

    Area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,15 +14,6 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ Constructor method """
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        elif type(height) != int:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
             self.__height = height
             self.__width = width
 

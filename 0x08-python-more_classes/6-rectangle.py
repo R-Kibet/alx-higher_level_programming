7ee737d6c2049ef73ba1b71368029ce26f1f9401
root <richkibz@gmail.com>
2023-01-03 21:19:19 +0300

    refix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.__size
+        return self.__height
 
     @height.setter
     def height(self, value):
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.__size
+        return self.__height
 
     @height.setter
     def height(self, value):

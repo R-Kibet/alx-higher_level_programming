5fb39db2f89f8483ffe37396c41730182583cf97
root <richkibz@gmail.com>
2023-01-03 22:33:18 +0300

    instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,12 +11,12 @@ class Rectangle:
     """
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Constructor method """
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
-        
 
     @property
     def width(self):

cbb97bee56e3f02a8e52fadad534989605b39b30
root <richkibz@gmail.com>
2023-01-03 22:07:14 +0300

    eval magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -52,6 +52,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
+
     def __str__(self):
         """ Return string to print rectangle with # """
         if self.__width == 0 or self.__height == 0:

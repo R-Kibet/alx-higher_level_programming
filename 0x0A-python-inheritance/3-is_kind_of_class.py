e53ada76502da75d6cac67dccd6cf8c6ca9f8d1e
root <richkibz@gmail.com>
2023-01-09 17:45:25 +0300

    instance of class inherited from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" object intane of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ function that shows oject is insrance of a class
+
+        Args:
+            obj- object
+             a_class - class
+    """
+
+    return isinstance(obj, a_class)

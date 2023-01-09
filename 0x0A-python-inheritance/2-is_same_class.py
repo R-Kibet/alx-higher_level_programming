230725ea9f082b2f8d4573c6fd326576cc1f4e91
root <richkibz@gmail.com>
2023-01-09 17:37:34 +0300

    object instance of class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" object instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """ a funcrion that return true or false
+
+        Args:
+            obj - object
+            a_class- class
+    """
+
+    if isinstance(type(obj), a_class):
+        return True
+    return False

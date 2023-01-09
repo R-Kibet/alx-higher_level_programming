cae2aa893dd455f0bd12d9fbf0a5489a18f00f68
root <richkibz@gmail.com>
2023-01-09 18:40:07 +0300

    rectangle inheriting from basegeometry

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+class rectangle inheriting from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle that inherits from BaseGeomtry """
+
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator(width, height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ method """
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)

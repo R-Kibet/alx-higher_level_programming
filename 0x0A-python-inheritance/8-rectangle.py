decad52fb07bd656020f48bb49d583bbccbdffbb
root <richkibz@gmail.com>
2023-01-09 18:32:26 +0300

    class inhering from another cals

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class rectangle inheriting from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle that inherits from BaseGeomtry """
+
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator(width, height)
+        self.__width = width
+        self.__height = height

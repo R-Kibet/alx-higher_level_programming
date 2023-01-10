fdad8bf8f144280f1d5ed15361b0c62e26d3cc41
root <richkibz@gmail.com>
2023-01-10 08:31:45 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhton3
+""" pascal triangle """
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers rep the Pascal’s triangle """
+
+    t = []
+    if n == 0:
+        return t
+
+    t.append([1])
+
+    for i in range(1, n):
+        prev = t[-1]
+        nxt = [1]
+        for i in range(len(prev) - 1):
+            nxt.append(prev[i] + prev[i + 1])
+        nxt += [1]
+        t.append(nxt)
+
+    return t

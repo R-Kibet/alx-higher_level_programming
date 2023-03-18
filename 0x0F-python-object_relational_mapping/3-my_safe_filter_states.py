0b7f2fc39424aa6b016a3956cf195ee55fad3818
root <richkibz@gmail.com>
2023-03-18 17:57:15 +0300

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    connection to database use argv to hide crucial data
+    """
+
+    engine = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+            )
+
+    crs = engine.cursor()
+    crs.execute('SELECT * from states WHERE name = %s ORDER BY states.id',
+                (argv[4], ))
+    r = crs.fetchall()
+
+    for i in r:
+        print(i)
+
+    crs.close()
+    engine.close()

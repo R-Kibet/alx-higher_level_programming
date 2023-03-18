1a5e05ca6aaa68f5e1a0287a385897877afe2106
root <richkibz@gmail.com>
2023-03-18 18:42:53 +0300

    filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sqlalchemy
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+def select():
+    """
+    contains 'a'
+    """
+
+    st = session.query(State).filter(
+            State.name.contains('a')).order_by(State.id)
+    if st is None:
+        print("Nothing")
+    for i in st:
+        print('{}: {}'.format(i.id, i.name))
+
+
+select()

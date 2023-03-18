aedf5dc2704306d823478f9c0c13816b08a60c00
root <richkibz@gmail.com>
2023-03-18 17:21:53 +0300

    list all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    connection to database use argv to hide crucial data
+    """
+
+    engine = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+            )
+
+    crs = engine.cursor()
+    query = "SELECT * FROM states"
+    crs.execute(query)
+    r = crs.fetchall()
+
+    for i in r:
+        print(i)

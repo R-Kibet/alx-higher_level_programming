f474230127cc87a20bbeb467c5a3a4694b6ec0f4
root <richkibz@gmail.com>
2023-04-25 22:08:28 +0300

    print charact

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-
 const request = require('request');
 const url = 'https://swapi.co/api/films/' + process.argv[2];
-
-request(url, (err, response, body) => {
-  if (!err) {
-    const result = JSON.parse(body).characters;
-    result.forEach((character) => {
-      request(character, (err, response, body) => {
-        if (response) {
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
           console.log(JSON.parse(body).name);
-        } else {
-          console.log(err);
         }
       });
     });
